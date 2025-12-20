34d472282c4386fef59b6381e46a8dba2f38e241
Ian Elliott <ie2173@columbia.edu>
2025-12-20 14:54:25 -0800

    next update

--- src/models/deals.ts
+++ src/models/deals.ts
@@ -13,7 +13,14 @@ const dealSchema = new mongoose.Schema({
   disputeWinner: { type: mongoose.Schema.Types.ObjectId, ref: "User" },
   status: {
     type: String,
-    enum: ["open", "completed", "cancelled", "disputed", "claimed"],
+    enum: [
+      "open",
+      "completed",
+      "cancelled",
+      "disputed",
+      "claimed",
+      "proof_uploaded",
+    ],
     default: "open",
   },
   sellerProof: {
