fe9a0b7add060d4ffc1ee81fe0fc047bec61be82
Ian Elliott <ie2173@columbia.edu>
2025-12-23 11:56:17 -0800

    expanded the rate limiter

--- src/middleware/rateLimiter.ts
+++ src/middleware/rateLimiter.ts
@@ -2,7 +2,7 @@ import rateLimit from "express-rate-limit";
 
 export const apiLimiter = rateLimit({
   windowMs: 15 * 60 * 1000,
-  max: 100,
+  max: 200,
   message: "Too many requests from this IP, please try again later.",
   standardHeaders: true,
   legacyHeaders: false,
@@ -10,7 +10,7 @@ export const apiLimiter = rateLimit({
 
 export const blockchainCallLimiter = rateLimit({
   windowMs: 60 * 1000,
-  max: 1,
+  max: 10,
   message: "Too many blockchain calls, please try again later.",
   standardHeaders: true,
   legacyHeaders: false,
@@ -18,7 +18,7 @@ export const blockchainCallLimiter = rateLimit({
 
 export const authLimiter = rateLimit({
   windowMs: 15 * 60 * 1000,
-  max: 5,
+  max: 150,
   message: "Too many authentication attempts, please try again later.",
   standardHeaders: true,
   legacyHeaders: false,
